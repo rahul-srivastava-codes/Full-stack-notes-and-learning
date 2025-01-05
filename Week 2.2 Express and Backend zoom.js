a14d4edc9f3a74d10f363382a07002254da8f5e0
black-cat-developer <rahulsrichunar@gmail.com>
2025-01-05 16:19:39 +0530

    first commit

---
+++ Week 2.2 Express and Backend zoom.js
@@ -0,0 +1,5 @@
+// What is Node.js ?
+// Node.js is a free, open-source JavaScript runtime environment that allows developers to run JavaScript code outside of a web browser
+
+// What is bun.js ?
+// Written in zig and similar to node.js
