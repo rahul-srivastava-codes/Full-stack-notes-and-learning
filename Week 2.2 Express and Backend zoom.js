243362376c6c826a71f09fee3630dad5093aa1a8
Rahul Srivastava <117383410+rahul-srivastava-codes@users.noreply.github.com>
2025-01-06 16:40:56 +0530

    Update Week 2.2 Express and Backend zoom.js

--- Week 2.2 Express and Backend zoom.js
+++ Week 2.2 Express and Backend zoom.js
@@ -3,3 +3,18 @@
 
 // What is bun.js ?
 // Written in zig and similar to node.js
+ What is express?
+Express is a powerful and popular web framework for Node.js that simplifies the process of building web applications and APIs. 
+some key points about Express:
+Minimalist Framework: Express is designed to be unopinionated and minimalist, allowing developers to structure their applications as they see fit.
+Robust Features: It provides a robust set of features for web and mobile applications, including:
+  Routing: Easily define routes for your application.
+  Middleware: Use middleware functions to handle requests and responses.
+  Template Engines: Integrate with various template engines for dynamic content rendering.
+  Open Source: Express is open-source, which means it's free to use and has a large community contributing to its development.
+RESTful APIs: It's particularly well-suited for building RESTful APIs, making it a go-to choice for many developers.
+
+Note : How to access it using the code
+Simply in search bar write localhost:3000?n=3
+  and can catch code in const p = req.query.n
+
